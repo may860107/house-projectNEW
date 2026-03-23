ef036aaf38e4d5afd5afed656814be134d7d66f5
may860107 <may860107@gmail.com>
2026-03-23 18:26:49 +0800

    Delete sanity.ts

--- sanity.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'next-sanity';
-
-export const client = createClient({
-  projectId: 'dd04z9gp', 
-  dataset: 'production',
-  apiVersion: '2024-03-22', 
-  useCdn: true, 
-});
